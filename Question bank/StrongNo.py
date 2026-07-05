2f288ecb0adf10548db4f82bbc0e9448e12fb916
vaishnaviwaidande21-commits <vaishnaviwaidande21@gmail.com>
2026-07-05 22:41:16 +0530

    Create StrongNo.py

---
+++ Question bank/StrongNo.py
@@ -0,0 +1,18 @@
+num = int(input("Enter a number: "))
+temp = num
+sum = 0
+
+while num > 0:
+    digit = num % 10
+    fact = 1
+
+    for i in range(1, digit + 1):
+        fact = fact * i
+
+    sum = sum + fact
+    num = num // 10
+
+if sum == temp:
+    print("Strong Number")
+else:
+    print("Not a Strong Number")
