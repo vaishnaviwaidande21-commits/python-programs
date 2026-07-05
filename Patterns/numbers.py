8f73e52dd9dbe51f2e08b77c611b80de02550db4
vaishnaviwaidande21-commits <vaishnaviwaidande21@gmail.com>
2026-07-05 18:32:10 +0530

    Create numbers.py

---
+++ Patterns/numbers.py
@@ -0,0 +1,7 @@
+for i in range(3): #2
+    for j in range(i+1): # 3                                       #1
+                                                                   #3 3
+                                                                   #5 5 5
+            
+        print(2 * i + 1 ,end = " ")
+    print()
