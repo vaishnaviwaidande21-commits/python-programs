daf46a6ccc95b929defcde7b821b6c40d4495083
vaishnaviwaidande21-commits <vaishnaviwaidande21@gmail.com>
2026-07-05 18:02:10 +0530

    Basic.py

--- String/Basic.py
+++ String/Basic.py
@@ -1,1 +1,20 @@
+name = "ram"
+print(name)
 
+print(name[0])
+print(id(name))
+
+print(name[1:3])
+
+#slicing-> vname[startindex : endindex]
+new_name = "p" + name[1:3]
+print(new_name)
+print(id(new_name))
+
+str="Maharastra"
+
+print(str[6:10])
+print(str[0:4])
+print(str[::2])
+print(str[::-1])
+print(str[-1])
