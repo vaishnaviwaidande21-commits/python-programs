538256f77b65bd6c89a1e35874ee6e28e35d5095
vaishnaviwaidande21-commits <vaishnaviwaidande21@gmail.com>
2026-07-05 22:44:23 +0530

    Create PrimeNo.py

---
+++ Question bank/PrimeNo.py
@@ -0,0 +1,12 @@
+n = int(input("Enter a number: "))
+
+c = 0
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        c += 1
+
+if c == 2:
+    print("Prime Number")
+else:
+    print("Not Prime Number")
